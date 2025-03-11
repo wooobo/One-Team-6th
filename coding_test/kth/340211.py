4de35f55b0b1a275d73a8cce8fccac7eb416050f
slowin <irooori89@gamil.com>
2025-03-11 14:30:50 +0900

    feat: add code 340211 2차 commit
    출발지점 포함하도록 해야하는 이슈 있음

--- coding_test/kth/340211.py
+++ coding_test/kth/340211.py
@@ -8,40 +8,45 @@ def get_key(index, position):
 
 
 def solution(points, routes):
-    answer = 0
     robot_paths = {}
 
-    for robot_num, target_robot_num in routes:
-        paths = []
+    for index, robot_routes in enumerate(routes):
+        # 이동할 로봇 위치 및 포지션
+        robot_num = robot_routes[0]
         robot_position = points[robot_num - 1]
-        target_robot_position = points[target_robot_num - 1]
 
-        move_r = target_robot_position[0] - robot_position[0]
-        move_c = target_robot_position[1] - robot_position[1]
-
-        robot_last_position = robot_position
-        # move_r 부터 모두 이동
+        # 이동 경로 목표 지점
+        target_robot_nums = robot_routes[1:]
         time = 0
-        move_r_direction = 1 if move_r > 0 else -1
-        for i in range(1, abs(move_r) + 1):
-            robot_last_position = [robot_last_position[0] + move_r_direction, robot_last_position[1]]
-            key = get_key(time, robot_last_position)
-            time += 1
-            if key in robot_paths:
-                robot_paths[key] += 1
-            else:
-                robot_paths[key] = 1
+        for target_robot_num in target_robot_nums:
+            target_robot_position = points[target_robot_num - 1]
+            move_r = target_robot_position[0] - robot_position[0]
+            move_c = target_robot_position[1] - robot_position[1]
 
-        move_c_direction = 1 if move_c > 0 else -1
-        # move_c 부터 모두 이동
-        for i in range(1, abs(move_c) + 1):
-            robot_last_position = [robot_last_position[0], robot_last_position[1] + move_c_direction]
-            key = get_key(time, robot_last_position)
-            time += 1
-            if key in robot_paths:
-                robot_paths[key] += 1
-            else:
-                robot_paths[key] = 1
+            robot_last_position = robot_position
+            # move_r 부터 모두 이동
+            move_r_direction = 1 if move_r > 0 else -1
+            for i in range(1, abs(move_r) + 1):
+                robot_last_position = [robot_last_position[0] + move_r_direction, robot_last_position[1]]
+                key = get_key(time, robot_last_position)
+                time += 1
+                if key in robot_paths:
+                    robot_paths[key] += 1
+                else:
+                    robot_paths[key] = 1
+
+            move_c_direction = 1 if move_c > 0 else -1
+            # move_c 부터 모두 이동
+            for i in range(1, abs(move_c) + 1):
+                robot_last_position = [robot_last_position[0], robot_last_position[1] + move_c_direction]
+                key = get_key(time, robot_last_position)
+                time += 1
+                if key in robot_paths:
+                    robot_paths[key] += 1
+                else:
+                    robot_paths[key] = 1
+
+    print(robot_paths)
 
     return sum(1 for v in robot_paths.values() if v >= 2)
 
@@ -52,6 +57,6 @@ if __name__ == "__main__":
     # assert solution(points, routes) == 1
     # print(solution(points, routes))
     #
-    # print(solution([[3, 2], [6, 4], [4, 7], [1, 4]], [[4, 2], [1, 3], [4, 2], [4, 3]]))
+    print(solution([[3, 2], [6, 4], [4, 7], [1, 4]], [[4, 2], [1, 3], [4, 2], [4, 3]]))
 
-    print(solution([[2, 2], [2, 3], [2, 7], [6, 6], [5, 2]], [[2, 3, 4, 5], [1, 3, 4, 5]]	))
+    # print(solution([[2, 2], [2, 3], [2, 7], [6, 6], [5, 2]], [[2, 3, 4, 5], [1, 3, 4, 5]]	))
