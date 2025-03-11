4bb6469c4bdacc658cb1b0bbcbb5b0ecc4c92d85
slowin <irooori89@gamil.com>
2025-03-11 14:21:19 +0900

    feat: add code 340211 1차 commit

---
+++ coding_test/kth/340211.py
@@ -0,0 +1,57 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/340211
+from collections import Counter
+
+
+# 모든 로봇이 운송을 마칠 때까지 발생하는 위험한 상황의 횟수를 return 하도록 solution 함수를 완성해 주세요.
+def get_key(index, position):
+    return f"{index}_{position[0]}_{position[1]}"
+
+
+def solution(points, routes):
+    answer = 0
+    robot_paths = {}
+
+    for robot_num, target_robot_num in routes:
+        paths = []
+        robot_position = points[robot_num - 1]
+        target_robot_position = points[target_robot_num - 1]
+
+        move_r = target_robot_position[0] - robot_position[0]
+        move_c = target_robot_position[1] - robot_position[1]
+
+        robot_last_position = robot_position
+        # move_r 부터 모두 이동
+        time = 0
+        move_r_direction = 1 if move_r > 0 else -1
+        for i in range(1, abs(move_r) + 1):
+            robot_last_position = [robot_last_position[0] + move_r_direction, robot_last_position[1]]
+            key = get_key(time, robot_last_position)
+            time += 1
+            if key in robot_paths:
+                robot_paths[key] += 1
+            else:
+                robot_paths[key] = 1
+
+        move_c_direction = 1 if move_c > 0 else -1
+        # move_c 부터 모두 이동
+        for i in range(1, abs(move_c) + 1):
+            robot_last_position = [robot_last_position[0], robot_last_position[1] + move_c_direction]
+            key = get_key(time, robot_last_position)
+            time += 1
+            if key in robot_paths:
+                robot_paths[key] += 1
+            else:
+                robot_paths[key] = 1
+
+    return sum(1 for v in robot_paths.values() if v >= 2)
+
+
+if __name__ == "__main__":
+    points = [[3, 2], [6, 4], [4, 7], [1, 4]]
+    routes = [[4, 2], [1, 3], [2, 4]]
+    # assert solution(points, routes) == 1
+    # print(solution(points, routes))
+    #
+    # print(solution([[3, 2], [6, 4], [4, 7], [1, 4]], [[4, 2], [1, 3], [4, 2], [4, 3]]))
+
+    print(solution([[2, 2], [2, 3], [2, 7], [6, 6], [5, 2]], [[2, 3, 4, 5], [1, 3, 4, 5]]	))
